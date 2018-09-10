00b9e95d8daf89f31f41e3c0c91be27b99878e8d
SiddiqueKBM <kbmsiddique.official@gmail.com>
2018-09-10 16:14:50 -0400

    datastructure.UseArrayList Added

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,5 +1,10 @@
 package datastructure;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class UseArrayList {
 
 	public static void main(String[] args) {
@@ -9,6 +14,21 @@ public class UseArrayList {
 		 * Store all the sorted data into one of the databases.
 		 * 
 		 */
+		List<String> list = new ArrayList<String>();
+		list.add("AMI");
+		list.add("TUMI");
+		list.add("SE");
+		list.add("TAHARA");
+		list.remove(2);
+
+		Iterator it = list.iterator();
+		while(it.hasNext()){
+			System.out.println(it.next());
+		}
+
+		for (String X : list) {
+			System.out.println("Listed Names are -:" + X);
+		}
 	
 
 	}
